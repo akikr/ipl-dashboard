ad467f8826c528449b9902807e78ece0b11eaacb
akikr <ankit.kumar962@gmail.com>
2021-05-09 16:49:15 +0530

    Added: The UI elements needed for TeamPage, MatchDetailsCard & MatchSmallDetailsCard along with routing of pages

--- src/front-end/src/App.js
+++ src/front-end/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 
 function App() {
   return (
     <div className="App">
-     <TeamPage/>
+      <Router>
+        <Route path="/teams/:teamName">
+          <TeamPage />
+        </Route>
+      </Router>
     </div>
   );
 }
