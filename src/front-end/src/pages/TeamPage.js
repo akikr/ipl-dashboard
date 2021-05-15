4b8acc28873ead286e7680726ed155d00cbce4a0
akikr <ankit.kumar962@gmail.com>
2021-05-15 23:17:02 +0530

    Added: The TeamPage & MatchDetailsCards UI design along with scss style support

--- src/front-end/src/pages/TeamPage.js
+++ src/front-end/src/pages/TeamPage.js
@@ -2,6 +2,8 @@ import { React, useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
+import { PieChart } from 'react-minimal-pie-chart';
+import './TeamPage.scss';
 
 export const TeamPage = () => {
 
@@ -25,9 +27,25 @@ export const TeamPage = () => {
 
   return (
     <div className="TeamPage">
-      <h1>{team.teamName}</h1>
-      <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      <div className="team-name-section">
+        <h1 className="team-name">{team.teamName}</h1>
+      </div>
+      <div className="win-loss-section">
+        {'Wins / Losses'}
+        <PieChart
+          data={[
+            { title: 'Losses', value: team.totalMatches - team.totalWins, color: '#973831dc' },
+            { title: 'Wins', value: team.totalWins, color: '#0b815ac9' },
+          ]}/>
+      </div>
+      <div className="match-detail-section">
+        <h3>Latest Matches</h3>
+        <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      </div>
       {team.matches.slice(1).map(match => <MatchSmallCard key={match.id} teamName={team.teamName} match={match} />)}
+      <div className="more-link">
+        <a href="#">{'More >'}</a>
+      </div>
     </div>
   );
 }
